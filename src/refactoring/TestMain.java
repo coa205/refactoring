a380bd859558157a501b89b8fd187bec8c108fc9
coa205 <coa205@naver.com>
2017-03-21 10:58:47 +0900

    htmlStatement() 추가

--- src/refactoring/TestMain.java
+++ src/refactoring/TestMain.java
@@ -33,5 +33,10 @@ public class TestMain {
 		System.out.println(c1.statement());
 		System.out.println("=================================");
 		System.out.println(c2.statement());
+		
+		System.out.println("///////////////// html /////////////////");
+		System.out.println(c1.htmlStatement());
+		System.out.println("=================================");
+		System.out.println(c2.htmlStatement());
 	}
 }
